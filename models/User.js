5fa622887a428ad41c2fd31a77c2f4b7531ef54f
Tjerk Symens <98127222+tjerksymens@users.noreply.github.com>
2023-11-12 18:24:29 +0100

    fix: correct user model

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+    // user is a required string
+    user: {
+        type: String,
+        required: true,
+    },
+});
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
